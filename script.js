bfc40898254f591345636be51b715f4227aee1c7
Logan Woolf <loganwoolf@gmail.com>
2021-08-06 01:12:37 -0600

    populate cells with slider selection

---
+++ script.js
@@ -0,0 +1,17 @@
+const gridSlider = document.querySelector('#grid-size');
+const canvas = document.querySelector('.main-canvas');
+
+
+function drawCells() {
+	canvas.innerHTML = "";
+	const cellSize = (Math.trunc(canvas.offsetWidth)-1) / this.value;
+	for (let i = 0; i < this.value ** 2; i++) {
+		const cell = document.createElement('div');
+		cell.style.width = `${cellSize}px`;
+		cell.style.height = `${cellSize}px`;
+		cell.classList.add('cell');
+		canvas.appendChild(cell);
+	}
+}
+
+gridSlider.addEventListener('mouseup', drawCells);
